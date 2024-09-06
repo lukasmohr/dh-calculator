f16f10b9ae6a8558b6faa1af66c1775b9722d3d1
lukasmohr77 <lukas.mohr1997@gmail.com>
2024-09-06 07:44:20 +0200

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import RaceCalculator from './components/RaceCalculator'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <RaceCalculator />
     </main>
   )
